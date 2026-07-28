1c1a2d37816d1a0a0d86fa6231c600cce01b1ee5
Nech <nech@Nech-Mac.local>
2026-07-28 21:26:51 +0300

    Complete HW7

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('name-input');
+const greeting = document.getElementById('name-output');
+
+input.addEventListener("input", (event) => {
+    if (input.value.trim()) {
+        greeting.textContent = input.value.trim();
+    }
+});
---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+const body = document.body;
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', event => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
